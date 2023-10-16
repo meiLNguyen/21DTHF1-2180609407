8bf0a851d09261f88ad92749e6c30b16a2c192aa
Sucalbel <147361358+Sucalbel@users.noreply.github.com>
2023-10-16 08:51:33 +0700

    Update frmMain.cs

--- Lab04/frmMain.cs
+++ Lab04/frmMain.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 using System.Runtime.Remoting.Contexts;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using System.Reflection;
-
+// Hoang da o day
 namespace Lab04
 {
     public partial class frmMain : Form
